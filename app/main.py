66df66490b0e62a0d0b9d8ff8328b8ad8d1dd9f1
David Strejcek <strejcek.david.1@gmail.com>
2026-01-08 23:28:50 +0100

    Added dateutils to requirements, added checking of timestamp in ingest method, adjusted returns

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-import datetime
+from datetime import datetime
 import json
 import psycopg2
 import os
@@ -56,9 +56,17 @@ with app.app_context():
 
 @app.route("/ingest", methods=['POST'])
 def ingest():
-    received_at = datetime.datetime.now().astimezone().isoformat()
+    received_at = datetime.now().astimezone().isoformat()
     data = request.json
-    retMessage = {"status": "ok", "status_message": "Data succesfully recorded","timestamp": received_at}
+    retMessage = {"status": "ok", "status_message": "Data succesfully recorded", "timestamp": received_at}
+    
+    try:
+        parser.isoparse(data['timestamp'])
+    except (ValueError, TypeError):
+        retMessage["status"] = "error"
+        retMessage["status_message"] = "Invalid timestamp format. Use ISO 8601 with timezone."
+        return retMessage, 400
+
     try:
         cursor.execute("""
             INSERT INTO weather_readings (station_id, measured_at, received_at, temperature_c, humidity_pct, pressure_hpa)
@@ -67,11 +75,11 @@ def ingest():
             (data['station_id'], data['timestamp'], received_at, data['temperature_c'], data['humidity_pct'], data['press_hpa']
         ))
     except KeyError:
-        retMessage["status"] = "nok"
+        retMessage["status"] = "error"
         retMessage["status_message"] = "Invalid JSON format"
         return retMessage, 400
     except:
-        retMessage["status"] = "nok"
+        retMessage["status"] = "error"
         retMessage["status_message"] = "Unexpected error"
         return retMessage, 500
     return retMessage, 201
@@ -81,7 +89,7 @@ def latest():
     cursor.execute("SELECT * FROM weather_readings WHERE id=(SELECT max(id) FROM weather_readings);")
     data = cursor.fetchone()
     if data == None:
-        return "No data has yet been ingested", 503
+        return {}, 200
     retData = {
         "station_id": data[1],
         "measured_at": data[2],
