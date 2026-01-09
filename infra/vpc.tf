7f0f66d9ebae7eb1403811d1df6c7b154a2d6d4d
David Strejcek <strejcek.david.1@gmail.com>
2026-01-09 17:13:00 +0100

    Added back default values

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -16,15 +16,15 @@ resource "aws_subnet" "subnet_public" {
   }
 }
 
-# resource "aws_subnet" "subnet_private" {
-#   vpc_id            = aws_vpc.main.id
-#   cidr_block        = "10.0.2.0/24"
-#   availability_zone = "eu-north-1a"
+resource "aws_subnet" "subnet_private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "eu-north-1a"
 
-#   tags = {
-#     Name = "weatherapp-subnet-private"
-#   }
-# }
+  tags = {
+    Name = "weatherapp-subnet-private"
+  }
+}
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
